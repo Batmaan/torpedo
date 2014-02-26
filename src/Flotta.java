5cb790a8f7d0e508e63329014095a667bf449f68
Eszti <eszter.iklodi@gmail.com>
2014-02-26 21:52:21 +0100

    Update Flotta.java
    na itt van akk a végleges interface!!

--- src/Flotta.java
+++ src/Flotta.java
@@ -21,15 +21,37 @@ abstract public class Flotta {
 				index = i;
 				flotta.remove(index);
 				break;
+				}
 			}
-		}
+		
 	}
-	
+
+	// ebben kell inicializálni a flottát
+	// még nincs szabály, de max annyi hajót tegyetek bele, amennyit a kiírás alapján az elején kapunk
 	public abstract void init();
 	
+	// melyik hajókat és hogyan mozgassa a pálya az egyes körökben
+	// nem kötelező minden hajót mozgatni
+	// a hajókat egyesével lehet mozgatni a p.hajoMozgat(long ID, int merre) fg-nyel lehet
+	// implementációja: Palya.java 118.sor (jelenleg)
+	///////////
+	////345////
+	////206////
+	////187////
+	///////////
 	public abstract void flottaMozgat(Palya p);
 
+	// hová szeretnétek lőni
+	// lőni a p.lottek() fg meghívásával tudtok
+	// implementációja: Palya.java 216.sor (jelenleg)
+	// 0 : nem talált
+	// 1 : talált
+	// 2 : süllyedt
 	public abstract void lo();
 	
-	public abstract void generHajo(int HajoID, int HajoHossz);
+	// ezt a függvényt fogja meghívni a main, majd vmilyen algoritmus szerint (pl 10 körönként)
+	// azt jelenti, h kaptál egy új Hajót, a megadott ID-val és hosszal
+	// te döntöd el, hogy hova teszed, vagy h leteszed-e
+	public abstract void getNewHajo(long ID, int hossz);
+	
 }
